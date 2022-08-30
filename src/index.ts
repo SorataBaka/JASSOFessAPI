4a339dca4062644c93ee022d942dbd435e791879
Christian Harjuno <harjuno.tian@gmail.com>
2022-08-30 21:51:34 +0900

    fix: changed fetch to axios

--- src/index.ts
+++ src/index.ts
@@ -27,16 +27,13 @@ if (mongooseConnectionUrl === undefined)
 		"SETUP ERROR: MONGO_URL is not set in the environment variables"
 	);
 
-const csurfProtection = csurf({
-	cookie: true,
-	value: (req: Request) => {
-		return req.cookies["XSRF-TOKEN"];
-	},
-});
-
-app.use(bodyParser.json());
-app.use(express.json());
 app.use(errorhandler);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet());
+
 app.use(
 	cors({
 		allowedHeaders: [
@@ -44,8 +41,9 @@ app.use(
 			"X-Requested-With",
 			"Content-Type",
 			"Accept",
-			"X-CSRF-TOKEN",
+			"X-XSRF-TOKEN",
 			"XSRF-TOKEN",
+			"Cookie",
 		],
 		maxAge: 360000,
 		origin: trueOrigin,
@@ -55,12 +53,14 @@ app.use(
 		optionsSuccessStatus: 200,
 	})
 );
-app.use(helmet());
-app.use(cookieParser());
-app.use(csurfProtection);
-
-app.use(morgan("dev"));
-app.use(helmet());
+app.use(
+	csurf({
+		cookie: true,
+		value: (req: Request) => {
+			return req.cookies["XSRF-TOKEN"];
+		},
+	})
+);
 app.use("/api", versionRouter);
 
 app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
@@ -75,9 +75,9 @@ app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
 	return next();
 });
 
-app.all("/", csurfProtection, (req: Request, res: Response) => {
+app.all("/", (req: Request, res: Response) => {
 	res.cookie("XSRF-TOKEN", req.csrfToken(), {
-		maxAge: 36000000,
+		maxAge: 64000,
 		path: "/",
 	});
 	return res.json({
@@ -89,7 +89,17 @@ app.all("/", csurfProtection, (req: Request, res: Response) => {
 		},
 	});
 });
-
+// app.get("/authorize", (req: Request, res: Response) => {
+// 	return res.status(200).json({
+// 		status: 200,
+// 		isValid: true,
+// 		data: {
+// 			csrf: req.csrfToken(),
+// 			message: "OK",
+// 			code: "OK",
+// 		},
+// 	});
+// });
 app.listen(PORT, async () => {
 	const newConnection = await mongoose.connect(mongooseConnectionUrl);
 	if (newConnection.connections.length !== 0)
